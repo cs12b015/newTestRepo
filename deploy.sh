3653b9d74d77a46ddeaf252e709e73ab69bd7ab3
jayanth.k <jayanthk19@gmail.com>
2015-05-18 16:42:48 +0530

    for different paths

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,14 @@
 USER1="root"
 IP[0]="45.55.210.12"
 IP[1]="45.55.207.62"
+PATH1[0]="/home/code/newTestRepo"
+PATH1[1]="/home/code1/newTestRepo"
 
+COUNTER=0
 for i in ${IP[@]}; do
 	echo "connecting to the $i with $USER1"
-	ssh $USER1@$i 'bash -s' < temp2.sh
+	echo "PATH1=${PATH1[$COUNTER]}" >temp1.sh
+	cat temp2.sh >> temp1.sh
+	ssh $USER1@$i 'bash -s' < temp1.sh
+	let COUNTER=COUNTER+1
 done
--- temp2.sh
+++ temp2.sh
@@ -5,5 +5,5 @@ cd $PATH1
 PWD1=`pwd`
 echo "present working directory is $PWD1"
 PRESENT_IP=`ifconfig | grep 'inet addr:' | grep -v '127.0.0.1' | awk '{print $2}' `
-git pull || { echo "process stopped working ar $PRESENT_IP" ; exit 1;}
+git pull || { echo "process stopped working at $PRESENT_IP" ; exit 1;}
 echo "git pulling finnished"
